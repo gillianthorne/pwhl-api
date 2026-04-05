d63eaee3721d30d85b20d58554d8386a983c3190
ducky <gillianthorne1353@gmail.com>
2026-04-05 18:15:22 -0400

    rearranged stuff so we can now get individual events instead of only the entire game

--- app/routes/games.py
+++ app/routes/games.py
@@ -16,3 +16,15 @@ def get_db():
 @router.get("/{game_id}")
 def get_game(game_id:int, db=Depends(get_db)):
     return build_game_json(db, game_id)
+
+@router.get("/{game_id}/{event_type}")
+def get_event_type(game_id:int, event_type:str, db=Depends(get_db)):
+    events =  build_game_json(db, game_id)["events"]
+
+    filtered = [e for e in events if e["type"] == event_type]
+
+    if not filtered:
+        return {"error": "no events found or invalid type"}
+    
+    return filtered
+    
