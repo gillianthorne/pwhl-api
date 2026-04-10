89a180528ed2b1cf7c7b05dbf3b499b1403e86eb
ducky <gillianthorne1353@gmail.com>
2026-04-10 18:02:39 -0400

    teams route!

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.routes import games
+from app.routes import games, teams
 
 app = FastAPI()
 
 app.include_router(games.router, prefix="/games")
+app.include_router(teams.router, prefix="/teams")
