c61834865c7ded7711099c45e8b04df60934e8a3
ducky <gillianthorne1353@gmail.com>
2026-04-10 14:51:19 -0400

    changed how my database stores players and their respective teams so i changed this to reflect that

---
+++ app/model/current_players.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Date, ForeignKey, Integer, PrimaryKeyConstraint, String
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+class CurrentPlayers(Base):
+    id = Column(Integer, ForeignKey("players.id"), primary_key=True)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    jersey_number = Column(Integer)
+    position = Column(String)
+
+    player = relationship("Player")
+    team = relationship("Team")
---
+++ app/model/player_history.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Date, ForeignKey, Integer, PrimaryKeyConstraint, String
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+class PlayerHistory(Base):
+    id = Column(Integer, ForeignKey("players.id"))
+    start_date = Column(Date)
+    end_date = Column(Date)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    jersey_number = Column(Integer)
+    position = Column(String)
+
+    player = relationship("Player")
+    team = relationship("Team")
+
+    __table_args__ = (
+        PrimaryKeyConstraint("id", "start_date")
+    )
